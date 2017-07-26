6a8ee1674b6bab7b91770a3d44733c2526b8cb29
Ramkumar Ramalingam <rramalingam@planittesting.com>
2017-07-26 21:27:51 +0100

    test

---
+++ src/test/java/com/jft/MyFirstTest.java
@@ -0,0 +1,20 @@
+package com.jft;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rramalingam on 16/07/2017.
+ */
+public class MyFirstTest {
+    @Test
+    public void canAddTwoNumbers(){
+
+        int answer=2+2;
+        assertEquals("checck 2+2",answer,4);
+
+    }
+
+}
